874d82e5e3952980c962ed75fb596fed1d56dc5e
ian <me@iany.me>
2024-04-11 15:34:36 +0800

    use vscode for windows

--- .envrc.ps1
+++ .envrc.ps1
@@ -1,6 +1,6 @@
 $script:rememberPath = $env:PATH
 $devDir = if ( $env:DEV_DIR ) { $env:DEV_DIR } else { "C:\Dev" }
-$env:PATH += "$(pwd)\bin;$devDir\Bitcoin\daemon;$devDir\lnd-windows-amd64;"
+$env:PATH += ";$(pwd)\bin;$devDir\Bitcoin\daemon;$devDir\lnd-windows-amd64"
 
 function global:down {
   $env:PATH = $script:rememberPath
--- bin/start-daemons.ps1
+++
@@ -1,3 +0,0 @@
-wt -w 0 `
-  nt --title bitcoind -d . pwsh -nol -noe bin\start-bitcoind.ps1 `; `
-  sp --title lnd -d . pwsh -nol -noe bin\start-ingrid.ps1
